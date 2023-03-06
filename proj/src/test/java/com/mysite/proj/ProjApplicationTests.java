332d97fc2050175942c76e423fc8f169823ca37b
yerim park <qdf22333@hanmail.net>
2023-03-06 13:20:32 +0900

    23/03/06 - question

--- proj/src/test/java/com/mysite/proj/ProjApplicationTests.java
+++ proj/src/test/java/com/mysite/proj/ProjApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mysite.proj;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.proj.question.Question;
+import com.mysite.proj.question.QuestionRepository;
 
 @SpringBootTest
 class ProjApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+    @Test
+    void testJpa() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
+    }
 }
