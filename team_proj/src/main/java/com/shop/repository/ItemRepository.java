afea6a1348c3cc3da4b865b3e5233ee7650482ca
yerim park <qdf22333@hanmail.net>
2023-03-24 21:01:24 +0900

    23/03/24 메인페이지 수정중

--- team_proj/src/main/java/com/shop/repository/ItemRepository.java
+++ team_proj/src/main/java/com/shop/repository/ItemRepository.java
@@ -3,6 +3,8 @@ package com.shop.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shop.entity.Item;
 
@@ -10,5 +12,5 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 	
 	
 	
-
+	
 }
